5451704cfa62cc1d7e2be4c58d90206fe3874928
Lorenzo Mora <lorenzomora98@gmail.com>
2024-08-20 14:37:01 -0600

    this is for teams definitely

--- hackernewsproject.py
+++ hackernewsproject.py
@@ -1,32 +1,32 @@
-#importing necessary modules
+# Importing necessary modules
 import requests
 from bs4 import BeautifulSoup
 import pprint
 
-#listing urls of first two pages of Hacker News
+# Listing urls of first two pages of Hacker News
 urls = ['https://news.ycombinator.com/','https://news.ycombinator.com/?p=2']
 
-#acquiring links and subtext(for votes) of articles on page 1)
+# Acquiring links and subtext(for votes) of articles on page 1)
 res = requests.get(urls[0])
 soup = BeautifulSoup(res.content, 'html.parser')
 links = soup.select('.titleline > a')
 subtext = soup.select('.subtext')
 
-#acquiring links and subtext(for votes) of articles on page 2)
+# Acquiring links and subtext(for votes) of articles on page 2)
 res2 = requests.get(urls[1])
 soup2 = BeautifulSoup(res2.content, 'html.parser')
 links2 = soup2.select('.titleline > a')
 subtext2 = soup2.select('.subtext')
 
-#creating a superlist with information of both pages
+# Creating a superlist with information of both pages
 super_links = links + links2
 super_subtext = subtext + subtext2
 
-#sorting articles by votes
+# Sorting articles by votes
 def sort_stories_by_votes(hnlist):
     return sorted(hnlist, key= lambda k:k['votes'],reverse=True)
 
-#filtering articles by 100+ votes
+# Filtering articles by 100+ votes
 def create_custom_hackernews(links,subtext):
     hn = []
     for index,item in enumerate(links):
@@ -40,3 +40,4 @@ def create_custom_hackernews(links,subtext):
     return sort_stories_by_votes(hn)
 
 pprint.pprint(create_custom_hackernews(super_links, super_subtext))
+
